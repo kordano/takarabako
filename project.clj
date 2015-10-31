175bfaaefc9f0ac67d71154952e18df2421e0581
kordano <konrad.kuehne@rocketmail.com>
2015-10-31 17:29:03 +0100

    improve

--- project.clj
+++ project.clj
@@ -6,28 +6,21 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.cemerick/piggieback "0.2.1"]
+                 [org.clojure/tools.nrepl "0.2.11"]
                  [http-kit "2.1.18"]
                  [compojure "1.4.0"]
-                 [org.omcljs/om "1.0.0-alpha9"]
-                 [kioo "0.4.1"]
+                 [com.taoensso/carmine "2.12.0"]
+                 [cljsjs/react "0.14.0-1"]
+                 [org.omcljs/om "1.0.0-alpha11"]
                  [jarohen/chord "0.6.0"]
-                 [secretary "1.2.3"]
-                 [com.taoensso/carmine "2.12.0"]]
+                 ]
   :source-paths ["src/cljs" "src/clj"]
-  :profiles {:dev
-             {:dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]
-                             [cljsjs/react-with-addons "0.13.3-0"]
-                             [com.cemerick/piggieback "0.2.1"]
-                             [org.clojure/tools.nrepl "0.2.11"]
-                             [cljs-react-test "0.1.3-SNAPSHOT"]]
-              :plugins [[lein-midje "3.1.3"]
-                        [lein-figwheel "0.4.1"]
-                        [cljs-react-test "0.1.3-SNAPSHOT"]
-                        [lein-doo "0.1.5"]]}
-             :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-             :figwheel {:nrepl-port 7888
-                        :css-dirs ["resources/public/css"]}}
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :figwheel {:nrepl-port 7888
+             :css-dirs ["resources/public/css"]}
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.1"]]
   :clean-targets ^{:protect false} ["resources/public/js" "target" "out"]
   :cljsbuild {:builds
               {:dev
