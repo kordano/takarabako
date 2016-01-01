00e890b0653a30a2f99185e55fc14407bf1cbb26
Konrad Kühne <konrad.kuehne@rocketmail.com>
2016-01-01 17:01:58 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -1,30 +1,42 @@
 (defproject takarabako "0.1.0-SNAPSHOT"
+
   :description "FIXME: write description"
+
   :url "http://example.com/FIXME"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [http-kit "2.1.18"]
+                 [org.clojure/core.async "0.2.374"]
+                 [http-kit "2.1.19"]
                  [compojure "1.4.0"]
-                 [sablono "0.3.6"  :exclusions [cljsjs/react]]
+                 [sablono "0.5.3"  :exclusions [cljsjs/react]]
                  [org.omcljs/om "1.0.0-alpha14"]
-                 [io.replikativ/konserve "0.3.0-beta3"]
+                 [io.replikativ/konserve "0.3.0"]
                  [com.cemerick/piggieback "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.12"]
-                 [jarohen/chord "0.6.0"]]
+                 [jarohen/chord "0.7.0"]]
+
   :source-paths ["src/cljs" "src/clj"]
+
   :main takarabako.core
-  :plugins [[lein-figwheel "0.4.1"]
+
+  :plugins [[lein-figwheel "0.5.0-1"]
             [lein-sassy "1.0.7"]
-            [lein-cljsbuild "1.1.0"]]
+            [lein-cljsbuild "1.1.1"]]
+
   :sass {:src "resources/sass"
          :dst "resources/public"}
+
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
   :figwheel {:nrepl-port 7888
              :css-dirs ["resources/public"]}
+
   :clean-targets ^{:protect false} ["resources/public/js" "target" "out"]
+
   :cljsbuild {:builds
               {:dev
                {:source-paths ["src/cljs"]
